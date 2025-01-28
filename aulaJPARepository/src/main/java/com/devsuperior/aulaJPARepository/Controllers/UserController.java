18fdcb16917a5ed3a333811c8f47d22bebeb37c1
Gustavo Henrique <guazambujinha@gmail.com>
2025-01-28 16:19:52 -0300

    search by name add

--- aulaJPARepository/src/main/java/com/devsuperior/aulaJPARepository/Controllers/UserController.java
+++ aulaJPARepository/src/main/java/com/devsuperior/aulaJPARepository/Controllers/UserController.java
@@ -38,8 +38,20 @@ public class UserController {
     }
 
     @GetMapping(value = "/search-salary")
-    public ResponseEntity<Page<User>> searchBySalary(@RequestParam(defaultValue = "0") Double minSalary, @RequestParam(defaultValue = "1000000000000") Double maxSalary, Pageable pageable) {
+    public ResponseEntity<Page<User>> searchBySalary(
+        @RequestParam(defaultValue = "0") Double minSalary, 
+        @RequestParam(defaultValue = "1000000000000") Double maxSalary, 
+        Pageable pageable) {
     Page<User> result = userRepository.searchSalary(minSalary, maxSalary, pageable);
+
+    return ResponseEntity.ok(result);
+}
+
+    @GetMapping(value = "/search-name")
+    public ResponseEntity<Page<User>> searchByName(
+         @RequestParam(defaultValue = "") String name,
+         Pageable pageable) {
+    Page<User> result = userRepository.searchName(name, pageable);
 
     return ResponseEntity.ok(result);
 }
