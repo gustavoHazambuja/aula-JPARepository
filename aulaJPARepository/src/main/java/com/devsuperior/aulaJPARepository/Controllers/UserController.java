4daeaa41fa240c811ee47308aef60a3d72a83f71
Gustavo Henrique <guazambujinha@gmail.com>
2025-01-28 16:03:47 -0300

    search salary, pagination add

--- aulaJPARepository/src/main/java/com/devsuperior/aulaJPARepository/Controllers/UserController.java
+++ aulaJPARepository/src/main/java/com/devsuperior/aulaJPARepository/Controllers/UserController.java
@@ -3,9 +3,13 @@ package com.devsuperior.aulaJPARepository.Controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.aulaJPARepository.Entities.User;
@@ -16,13 +20,30 @@ import com.devsuperior.aulaJPARepository.Repositories.UserRepository;
 public class UserController {
     
     @Autowired
-    UserRepository userRepository;
+    private UserRepository userRepository;
 
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
     List<User> result = userRepository.findAll();
 
     return ResponseEntity.ok(result);
+    }
+
+    // Busca os usuários por página/paginação
+    @GetMapping(value = "/page")
+    public ResponseEntity<Page<User>> findAll(Pageable pageable) {
+    Page<User> result = userRepository.findAll(pageable);
+
+    return ResponseEntity.ok(result);
+    }
+
+    @GetMapping(value = "/search-salary")
+    public ResponseEntity<Page<User>> searchBySalary(@RequestParam(defaultValue = "0") Double minSalary, @RequestParam(defaultValue = "1000000000000") Double maxSalary, Pageable pageable) {
+    Page<User> result = userRepository.searchSalary(minSalary, maxSalary, pageable);
+
+    return ResponseEntity.ok(result);
 }
 
 }
+
+
--- aulaJPARepository/src/main/java/com/devsuperior/aulaJPARepository/Repositories/UserRepository.java
+++ aulaJPARepository/src/main/java/com/devsuperior/aulaJPARepository/Repositories/UserRepository.java
@@ -1,10 +1,16 @@
 package com.devsuperior.aulaJPARepository.Repositories;
 
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.aulaJPARepository.Entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     
-    
+    @Query("SELECT obj FROM User obj WHERE obj.salary >= :minSalary AND obj.salary <= :maxSalary")
+    Page<User> searchSalary(Double minSalary, Double maxSalary, Pageable pageable);
+
 }
