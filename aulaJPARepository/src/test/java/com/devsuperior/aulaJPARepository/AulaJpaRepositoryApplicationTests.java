5e58991e1d77927a7555a591a2ed7f43c7e28b73
Gustavo Henrique <guazambujinha@gmail.com>
2025-01-28 14:34:48 -0300

    fist commit

---
+++ aulaJPARepository/src/test/java/com/devsuperior/aulaJPARepository/AulaJpaRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.aulaJPARepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaJpaRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
