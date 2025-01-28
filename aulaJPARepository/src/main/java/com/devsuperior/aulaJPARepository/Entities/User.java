e77f801fe40599e49bcc4999f260091c7a1d3f70
Gustavo Henrique <guazambujinha@gmail.com>
2025-01-28 14:44:52 -0300

    sql and ORM add

--- aulaJPARepository/src/main/java/com/devsuperior/aulaJPARepository/Entities/User.java
+++ aulaJPARepository/src/main/java/com/devsuperior/aulaJPARepository/Entities/User.java
@@ -1,7 +1,17 @@
 package com.devsuperior.aulaJPARepository.Entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_users")
 public class User {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
