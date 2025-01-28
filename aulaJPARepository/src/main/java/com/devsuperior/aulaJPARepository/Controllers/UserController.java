ee1f2f275fd96ba63f924dc2e59e36a84fb04b98
Gustavo Henrique <guazambujinha@gmail.com>
2025-01-28 17:24:23 -0300

    method user add

--- aulaJPARepository/src/main/java/com/devsuperior/aulaJPARepository/Controllers/UserController.java
+++ aulaJPARepository/src/main/java/com/devsuperior/aulaJPARepository/Controllers/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -55,6 +57,11 @@ public class UserController {
 
     return ResponseEntity.ok(result);
 }
+
+    @PostMapping
+    public User addUser(@RequestBody User u){
+        return userRepository.save(u);
+    }
 
 }
 
